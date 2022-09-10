00d049809efd2dbd16f9c50f5b81ceedcec4186f
bll08 <izabellasantos424@gmail.com>
2022-09-09 22:35:43 -0300

    atualização

---
+++ atividade_pwII/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200&display=swap" rel="stylesheet">
+    <title>Tabela</title>
+</head>
+<?php
+
+?>
+<body>
+    <table id="tabela">
+    <tr>
+        <td class="linha_1"> Produto</td>
+        <td class="linha_1"> Valor</td>
+    </tr>
+    <tr>
+        <td class="linha"> Calça</td>
+        <td class="linha"> $$</td>
+    </tr>
+    <tr>
+        <td class="linha"> Blusa</td>
+        <td class="linha"> $$</td>
+    </tr>
+    <tr>
+        <td class="linha"> Camiseta</td>
+        <td class="linha"> $$</td>
+    </tr>
+    <tr>
+        <td class="linha"> Bermuda</td>
+        <td class="linha"> $$</td>
+    </tr>
+    <tr>
+        <td class="linha"> Moletom</td>
+        <td class="linha"> $$</td>
+    </tr>
+    <tr>
+        <td class="linha"> Cropped</td>
+        <td class="linha"> $$</td>
+    </tr>
+    <tr>
+        <td class="linha"> Saia</td>
+        <td class="linha"> $$</td>
+    </tr>
+    <tr>
+        <td class="linha"> Vetido</td>
+        <td class="linha"> $$</td>
+    </tr>
+    <tr>
+        <td class="linha"> Jaqueta</td>
+        <td class="linha"> $$</td>
+    </tr>
+    <tr>
+        <td class="linha"> Meia</td>
+        <td class="linha"> $$</td>
+    </tr> 
+
+    </table>
+</body>
+</html>
